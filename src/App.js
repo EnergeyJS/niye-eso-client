05bd339ddc01dff16957d8ea9450f6646490d0e8
Nuruzzamancse <mr.nuruzzamancse@gmail.com>
2019-06-21 01:53:04 +0600

    react material integrated

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import {Provider} from 'react-redux';
 import Counter from './views/Counter';
 import { TextBox }from './components/TextBox';
 import Counter2 from './views/Counter2';
+import Home from './views/Home';
 
 
 const App = () => (
   <Provider store={store}>
     <Router>
         <div>
-          <Route path="/" exact component={Counter} />
+          <Route path="/" exact component={Home} />
+          <Route path="/counter" exact component={Counter} />
           <Route path="/counter2" exact component={Counter2} />
           <Route path="/text/" component={TextBox} />
         </div>
---
+++ src/views/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Navbar from '../components/NavBar';
+export default function home(){
+    return <div>
+        <Navbar/>
+    </div>
+}
