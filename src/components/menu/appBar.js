b0ee73f2cd7a6eeef9e83fd7e35be40fcc7a2a8e
Nuruzzamancse <mr.nuruzzamancse@gmail.com>
2019-06-27 02:29:34 +0600

    updated

--- src/components/menu/appBar.js
+++ src/components/menu/appBar.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+/* eslint-disable react-hooks/rules-of-hooks */
+import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import MTStyle from '../../modules/index';
 
@@ -14,54 +15,130 @@ import Badge from '@material-ui/core/Badge';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import MoreIcon from '@material-ui/icons/MoreVert';
-import RenderMobileMenu from './mobileMenu';
-import DrawerMenuContext from '../context/DrawerMenuContext';
-import MainMenu from './mainMenu';
-import DrawerMenu from './drawerMenu';
+
+import { useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
 
 export default function appBar () {
   const classes = MTStyle ();
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const [open, setOpen] = React.useState (false);
-  /**From Nav */
-  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
   const [anchorEl, setAnchorEl] = React.useState (null);
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState (null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState (null);
 
+  const isMenuOpen = Boolean (anchorEl);
+  const isMobileMenuOpen = Boolean (mobileMoreAnchorEl);
 
   function handleProfileMenuOpen (event) {
     setAnchorEl (event.currentTarget);
+  }
+
+  function handleMobileMenuClose () {
+    setMobileMoreAnchorEl (null);
+  }
+
+  function handleMenuClose () {
+    setAnchorEl (null);
+    handleMobileMenuClose ();
   }
 
   function handleMobileMenuOpen (event) {
     setMobileMoreAnchorEl (event.currentTarget);
   }
 
-  const menuId = 'primary-search-account-menu';
-  const mobileMenuId = 'primary-search-account-menu-mobile';
 
-  function handleDrawerOpen () {
-    setOpen (true);
+  const menuId = 'primary-search-account-menu';
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{vertical: 'top', horizontal: 'right'}}
+      id={menuId}
+      keepMounted
+      transformOrigin={{vertical: 'top', horizontal: 'right'}}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{vertical: 'top', horizontal: 'right'}}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{vertical: 'top', horizontal: 'right'}}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton aria-label="Show 4 new mails" color="inherit">
+          <Badge badgeContent={4} color="secondary">
+            <MailIcon />
+          </Badge>
+        </IconButton>
+        <p>Messages</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton aria-label="Show 11 new notifications" color="inherit">
+          <Badge badgeContent={11} color="secondary">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <p>Notifications</p>
+      </MenuItem>
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          aria-label="Account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
+
+  function handleDrawerOpen() {
+    setOpen(true);
+  }
+
+  function handleDrawerClose() {
+    setOpen(false);
   }
 
   return (
     <div>
-      <AppBar
+    <AppBar
         position="fixed"
-        className={clsx (classes.appBar, {
+        className={clsx(classes.appBar, {
           [classes.appBarShift]: open,
         })}
       >
         <Toolbar>
-          <IconButton
+          <IconButton           
             color="inherit"
-            aria-label="Open drawer"
+            aria-label="Open drawer"    
             onClick={handleDrawerOpen}
             edge="start"
-            className={clsx (classes.menuButtonAppBar, {
+            className={clsx(classes.menuButtonAppBar, {
               [classes.hide]: open,
-            })}
+            })}        
           >
             <MenuIcon />
           </IconButton>
@@ -117,17 +194,46 @@ export default function appBar () {
           </div>
         </Toolbar>
       </AppBar>
-      <DrawerMenuContext.Provider
-        value={[mobileMoreAnchorEl, setMobileMoreAnchorEl]}
+      {renderMobileMenu}
+      {renderMenu}
+      <Drawer
+        variant="permanent"
+        className={clsx(classes.drawer, {
+          [classes.drawerOpen]: open,
+          [classes.drawerClose]: !open,
+        })}
+        classes={{
+          paper: clsx({
+            [classes.drawerOpen]: open,
+            [classes.drawerClose]: !open,
+          }),
+        }}
+        open={open}
       >
-        <RenderMobileMenu />
-      </DrawerMenuContext.Provider>
-      <DrawerMenuContext.Provider value={[anchorEl, setAnchorEl]}>
-        <MainMenu />
-      </DrawerMenuContext.Provider>
-      <DrawerMenuContext.Provider value={[open, setOpen]}>
-        <DrawerMenu />
-      </DrawerMenuContext.Provider>
+        <div className={classes.toolbar}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
     </div>
   );
 }
