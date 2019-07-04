30484dd2ef8485f04156f15bb267ac6e62c97040
Nuruzzamancse <mr.nuruzzamancse@gmail.com>
2019-07-05 02:07:56 +0600

    modal replaced

--- src/components/CartWrapper.js
+++ src/components/CartWrapper.js
@@ -6,54 +6,50 @@ import CardContent from '@material-ui/core/CardContent';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import {red} from '@material-ui/core/colors';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCartTwoTone'
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCartTwoTone';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
 import CartModal from '../components/menu/CartModal';
 
 const useStyles = makeStyles (theme => ({
-  avatar: {
-    backgroundColor: red[500],
-  },
-  discount: {
-    backgroundColor: red[500],
-    position: 'absolute',
+  root: {
     right: '0',
+    position: 'fixed',
+    'z-index': '1223',
+    top: '48vh',
   },
-  cart:{
-      position:'fixed',
-      right:'-30px',
-      top:"45%",
-      width:"100px",
-      height:'auto',
-      'z-index': '999',
-      background:'rebeccapurple'
+  paper: {
+    padding: theme.spacing (2),
+    textAlign: 'center',
+    backgroundColor: 'blue',
   }
 }));
 
-export default function CartWrapper() {
+export default function CartWrapper () {
   const classes = useStyles ();
   const [expanded, setExpanded] = useState (false);
   const [open, setOpen] = useState (false);
-
 
   function closeModal () {
     console.log ('Here clicked');
     setOpen (false);
   }
 
-  function openModal(){
-    setOpen(true);
+  function openModal () {
+    setOpen (true);
   }
 
-
   return (
-    <Card className={classes.cart} >      
-      <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
-        <ShoppingCartIcon onClick={() => openModal ()}/>
-        </Typography>
-      </CardContent>
-      <CartModal open={open} closeModal={closeModal}/>
-    </Card>
+    <div className={classes.root}>
+      <Grid container>
+        <Grid item xs style={{boxShadow: ''}}>
+          <CartModal open={open} closeModal={closeModal}/>
+        </Grid>
+        <Grid item>
+          <Paper className={classes.paper} onClick={openModal} style={{cursor: 'pointer'}}><ShoppingCartIcon/></Paper>
+        </Grid>
+      </Grid>
+    </div>
   );
 }
--- src/components/menu/CartModal.js
+++ src/components/menu/CartModal.js
@@ -1,61 +1,30 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import Modal from '@material-ui/core/Modal';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-import Button from '@material-ui/core/Button';
-
-
-function getModalStyle () {
-  const top = 50;
-  const left = 50;
-
-  return {
-    top: `${top}%`,
-    left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`,
-  };
-}
-
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
 const useStyles = makeStyles (theme => ({
-  cartContainer:{
-    position:'relative'
+  cartContainer: {
+    position: 'relative',
   },
-  cart:{
-    position:'absolute',
-    width:'400px',
-    top:'45vh',
-    right:'0'
-  }
+  cart: {
+    position: 'absolute',
+    width: '400px',
+    top: '45vh',
+    right: '0',
+  },
 }));
 
 export default function SimpleModal({open = false, closeModal, data = {}}) {
-  // getModalStyle is not a pure function, we roll the style only on the first render
-  const [modalStyle] = useState (getModalStyle);
-
-  const [values, setValues] = useState ({
-    age: '',
-    name: 'hai',
-  });
-
-  function handleChange (event) {
-    setValues (oldValues => ({
-      ...oldValues,
-      [event.target.name]: event.target.value,
-    }));
-  }
-
   const classes = useStyles ();
-  let classModal = classes.paper;
   return (
-    <div>
-      <div >
-        <h1>HERe</h1>
-      </div>
-    </div>
+        <Card>
+          { open && <CardContent>
+            <button onClick={closeModal}>Close</button>
+            Hi there
+          </CardContent>
+          }
+        </Card>
   );
 }
